7db08ae81afee02a10371ba8ee0fb637ce69c724
Juan Paez <jpaezg93@gmail.com>
2023-10-13 14:56:56 -0500

    Adding a gitignore file and adding a jupiternotebook with initial analysis

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ import pandas as pd
 def main():
     dataset = load_dataset("mstz/heart_failure", split="train")
     df = pd.DataFrame(dataset)
-    #print(df.columns)
     deceased = df[df['is_dead']==1]
     survived = df[df['is_dead']==0]
     print(f'Participants who passed away throughout the study on average were {deceased["age"].mean():.2f} years old')
-    print(f"Participants who were alive during the study's duration had an average age of {survived['age'].mean():.2f}")
+    print(f"Participants who stayed alive during the study's duration had an average age of {survived['age'].mean():.2f}")
+    
+    #Exploratory Data Analysis
+    print(df.info())
+    print(df.describe())
+    
 if __name__ == '__main__':
     main()
+
